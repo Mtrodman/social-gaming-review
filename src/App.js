bb31615bb3c58b0b931a2e682ee8fef396a6449a
matt rodman <mrodman89@gmail.com>
2023-01-11 19:29:05 -0600

    Created Components and code for the forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BlogForm} from './Components/BlogForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <div className="navbar">
+            <navbar/>
+        </div>
+        <div className="GameBody">
+          <h1>
+            Welcome To Game Rater 
+          </h1>
+          <BlogForm/>
+        </div>
+      </div>
   );
 }
 
